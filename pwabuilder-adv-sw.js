65aaac4f91d7abb98ee14727d14dd0f6aadccdc1
devmnl <https://github.com/devmnl>
2024-08-06 21:04:14 -0300

    pwa builder config

---
+++ pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"b6040b445d7127e0c935d2d629f77ee2","url":"css/style.css"},{"revision":"ac7cbcf7889529404896b0eea2b1e8c6","url":"index.html"},{"revision":"fe7468e8eede8bbcee950cc5e5a9d047","url":"js/script.js"}]);
